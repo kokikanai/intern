daf5b3820970630b8894c9f19238f7564e5098a5
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-08-31 11:25:26 +0900

    3

--- a.py
+++ a.py
@@ -2,7 +2,7 @@ def is_leap_year(year):
     if year % 4 == 0:
         if year % 100 != 0:
             return True
-        else if year % 400 == 0:
+        elif year % 400 == 0:
             return True
     return False
 
