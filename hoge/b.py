ba7e4ab71895dd52008b6761b5c8e1daebfbf7c5
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-08-31 13:24:20 +0900

    18
    perfect

--- hoge/b.py
+++ hoge/b.py
@@ -1,10 +1,10 @@
 class b:
     def f(x):
         return x*x - 4.0
-     
+
     def df(x):
         return 2.0*x
-        
+
     def newton_method(a, eps):
         for i in range(1000):
             ah = a - f(a)/df(a)
