c1f5fb715d7835998b0ff3f77d9826b0dcdd4bbf
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-08-31 11:32:02 +0900

    6

--- a.py
+++ a.py
@@ -2,8 +2,13 @@ def is_leap_year(year):
     if year % 4 == 0:
         if year % 100 != 0:
             return True
-        if year % 400 == 0:
+        elif year % 400 == 0:
             return True
+        elif year % 100 == 0:
+            return False
     return False
 
+print is_leap_year(2000)
 print is_leap_year(2001)
+print is_leap_year(2004)
+print is_leap_year(2100)
