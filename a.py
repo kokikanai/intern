e071b7852d099b1b6d95d89de48e68f684452a1f
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-09-04 16:07:04 +0900

    g

--- a.py
+++ a.py
@@ -6,8 +6,5 @@ class a:
             elif year % 100 == 0:
                 return True
         return False
-
-    print (is_leap_year(2000))
-    print (is_leap_year(2001))
     print (is_leap_year(2004))
     print (is_leap_year(2100))
