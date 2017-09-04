64e64cfeb84052b6970886b8760dc2c636160d8f
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-09-04 16:32:17 +0900

    j

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ class a:
         if year % 4 == 0:
             if year % 100 == 0:
                 return True
-            elif year % 100 == 0:
+            elif year % 400 == 0:
                 return True
         return False
 
