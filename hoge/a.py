07f2e0f65c326d66da6994c09ad0212736f5a4e5
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-09-04 15:29:19 +0900

    s

--- hoge/a.py
+++ hoge/a.py
@@ -1,7 +1,8 @@
 class a:
     def is_leap_year(year):
         if year % 4 == 0:
-            if year % 100 == 0:return True
+            if year % 100 == 0:
+                return True
             elif year % 100 == 0:
                 return True
         return False
